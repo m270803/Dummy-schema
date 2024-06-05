33e8c0e6e1a7b6ef7a8fe74a4547dc6b48ea6952
Mehul Sharma <sharma.mehul8448@gmail.com>
2024-06-05 18:01:30 +0530

    .

--- config/database.js
+++ config/database.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/Dummy_schema', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
-  }
+    try {
+        mongoose.set('strictQuery', true);
+        
+        const uri = 'mongodb+srv://vikramviks0802:ei6kTjyQQjnoletR@cluster0.kalkm7h.mongodb.net/?retryWrites=true&w=majority';
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        
+        console.log('DB Connected');
+    } catch (err) {
+        console.error('Error connecting to the database', err);
+    }
 };
 
 module.exports = connectDB;
